2d17df53533ab9c341bea0b36fa1a281dd4aee0f
ossian <omapes13@gmail.com>
2017-08-16 13:27:24 -0400

    init

---
+++ MapLocations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TLDKit;
+using Harmony;
+using UnityEngine;
+
+namespace ShowMapLocation {
+    [HarmonyPatch(typeof(Panel_Map))]
+    [HarmonyPatch("LoadMapElementsForScene")]
+    public class ShowMapLocation {
+        public static void Postfix(Panel_Map __instance, String sceneName) {
+            GameObject cacheObj = MapManager.LocateSceneByName("PrepperCache");
+            if (cacheObj) {
+                MapManager.AddToMap(__instance, sceneName, "Cache", "ico_NoTool", cacheObj.transform.localPosition);
+            }
+            MapManager.AddToMap(__instance, sceneName, "GAMEPLAY_Location", "ico_X", GameManager.GetPlayerTransform().localPosition);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Map Locations
+
+This is a mod for **The Long Dark** by Hinterland Games Studio, Inc.
+
+
+This mod will add the location of the player and the location of the Prepper Cache to the map.
+
+
+Requirements:
+ - [Mod Loader](https://github.com/zeobviouslyfakeacc/ModLoaderInstaller)
+ - [TLDKit](https://github.com/The5heepDev/TLDKit)
